bc4ca8c68e6247b8195ba1cba1be8a47e690c032
SRIHARI-MG <srihari.vb@mind-graph.com>
2024-10-02 15:23:57 +0530

    updated: layout page

--- src/layout/LayoutPage.tsx
+++ src/layout/LayoutPage.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Header from "@/layout/Header";
 import { ThemeProvider } from "@/hooks/use-theme";
+import { useOutlet } from "react-router-dom";
 
 interface LayoutProps {
   role?: string | string[];
-  children?: React.ReactNode;
 }
 
-export function LayoutPage({ role, children }: LayoutProps) {
+export function LayoutPage({ role }: LayoutProps) {
+  const outlet = useOutlet();
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-background">
         <Header />
-        <main className="container mx-auto py-6 px-4"></main>
+        <main className="container mx-auto py-6 px-4">{outlet}</main>
       </div>
     </ThemeProvider>
   );
