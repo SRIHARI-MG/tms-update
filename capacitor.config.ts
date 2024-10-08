3b63eccfb20a71ab06192e783095efc81083d01a
SRIHARI-MG <srihari.vb@mind-graph.com>
2024-10-08 16:12:49 +0530

    added: android packages

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mindgraph.tms',
+  appName: 'Mindgraph TMS',
+  webDir: 'dist',
+
+};
+
+export default config;
