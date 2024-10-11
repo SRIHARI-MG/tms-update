bb850d5006cba227fe8301b511f96568bcc90406
SRIHARI-MG <srihari.vb@mind-graph.com>
2024-10-11 14:07:06 +0530

    minor changes

--- src/pages/private/Employee_Components/EmployeeDashboard.tsx
+++ src/pages/private/Employee_Components/EmployeeDashboard.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const EmployeeDashboard = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <MindgraphNetwork width={450} height={250} nodeScale={1} />
+      <p className="text-3xl text-primary/30 font-semibold">Comming ... soon</p>
     </div>
   );
 };
